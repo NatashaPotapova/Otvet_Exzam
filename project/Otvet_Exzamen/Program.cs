8585b036a628233189caf9c3b3088fbe78af6824
NatashaPotapova <natusi-1999@mail.ru>
2020-04-25 14:25:32 +0300

    Готовая программа

--- project/Otvet_Exzamen/Program.cs
+++ project/Otvet_Exzamen/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Classes;
+using System.IO;
 
 namespace Otvet_Exzamen
 {
@@ -10,6 +12,66 @@ namespace Otvet_Exzamen
     {
         static void Main(string[] args)
         {
+                int N = 0;
+                Console.WriteLine("Введите размер массива");
+                N = Convert.ToInt32(Console.ReadLine());
+                Route[] Routes = new Route[N];
+
+                for (int i = 0; i < N; i++)
+                {
+                    Routes[i] = new Route();
+                    Console.WriteLine("Введите номер троллейбуса ");
+                int Number;
+                Int32.TryParse(Console.ReadLine(), out Number);
+                Routes[i].Number = Number;
+                Console.WriteLine("Введите количество остановок ");
+                int Quantity;
+                Int32.TryParse(Console.ReadLine(), out Quantity);
+                Routes[i].Quantity = Quantity;
+                    Console.WriteLine("Введите время в пути в минутах ");
+                int Time;
+                Int32.TryParse(Console.ReadLine(), out Time);
+                Routes[i].Time = Time;
+
+                }
+
+                foreach (var i in Routes)
+                {
+                    Console.WriteLine("Номер троллейбуса " + i.Number + " Количество остановок " + i.Quantity + " Время в пути " + i.Time + "\n");
+                }
+            SaveFile(Routes);
+            Sortirovka(Routes);
+            Console.ReadKey();
+            Route[] Sortirovka(Route[] routes)
+            {
+                Route[] SortirovkaRoutes = Routes.AsQueryable<Route>().OrderBy(rout => rout.Quantity).ThenBy(rout => rout.Time).ToArray();
+                return SortirovkaRoutes;
+            }
         }
+        public static void SaveFile(Route[] Routes)
+        {
+            using (StreamWriter wrt = new StreamWriter("Car.txt"))
+            {
+                foreach (var i in Routes)
+                {
+                    wrt.WriteLine("Номер троллейбуса " + i.Number + " Количество остановок " + i.Quantity + " Время в пути " + i.Time + "\n");
+                }
+            }
+        }
+        private static bool Proverka(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= '0' && str[i] <= '9')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
+            
+    
+
---
+++ project/Otvet_Exzamen/Route.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Route
+    {
+        private int number;
+        private int quantity;
+        private int time;
+
+        public int Number { get => number; set => number = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+        public int Time { get => time; set => time = value; }
+
+        public Route(int number, int quantity, int time)
+        {
+            this.Number = number;
+            this.Quantity = quantity;
+            this.Time = time;
+        }
+        public Route()
+        {
+            this.Number = 0;
+            this.Quantity = 0;
+            this.Time = 0;
+        }
+        public override string ToString()
+        {
+            return "Номер маршрута:" + Number + "\nКоличестово остановок:" + Quantity + "\nВремя в пути:" + Time + "\n";
+        }
+    }
+}
